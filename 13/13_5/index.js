5cd0c5406a9db4e270662dfba4a167c48e062786
pflaz <pf@szach.net>
2018-07-04 22:42:38 +0200

    13.5 - colors module

---
+++ 13/13_5/index.js
@@ -0,0 +1,25 @@
+var os = require('os');
+var OSinfo = require('./modules/OSinfo');
+
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.toString().trim();
+		switch (instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/version':
+				process.stdout.write(process.version);
+				break;
+			case '/getOSinfo':
+				OSinfo.print();
+				break;
+			default:
+				process.stderr.write('Wrong instruction!\n');
+		}
+	}
+});
+
