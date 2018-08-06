352511356495e27f387e50253e18539a0d6964be
pflaz <pf@szach.net>
2018-08-06 16:37:43 +0200

    20.4

---
+++ 20/20_3, 20_4/country-app/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {Router, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store';
+import routes from './routes';
+
+import DevTools from './store/DevTools';
+import { getCountries } from './actions/actions-countries';
+
+store.dispatch(getCountries());
+render(
+    <Provider store={store}>
+        <div>
+            <Router history={hashHistory} routes={routes}>
+
+            </Router>
+            <DevTools />
+        </div>
+    </Provider>, 
+    document.getElementById('root')
+);
+
