619f72040687c2d7cc9f789c82062c8d1dacc68c
pflaz <pf@szach.net>
2018-08-17 05:01:30 +0200

    23.4

--- 23/23_4/kanban-app/server/routes/lane.routes.js
+++ 23/23_4/kanban-app/server/routes/lane.routes.js
@@ -3,5 +3,16 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
+// Add new Lane
+router.route('/lanes').post(LaneController.addLane);
+
+// Get all lines
+router.route('/lanes').get(LaneController.getLanes);
+
+// Delete a lane by laneId
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
+
+// Change lane's name
+router.route('/lanes').put(LaneController.updateLane);
 
 export default router;
